2395f31df5e9c2b10d7a6f54f41f70fa76e81c07
ClaireYarber <clyarber@ou.edu>
2024-12-09 11:14:22 -0600

    Create book.php

---
+++ model/view/books-by-author/book.php
@@ -0,0 +1,8 @@
+  <tr>
+    <td><?php echo $book['book_id']; ?></td>
+    <td><?php echo $book['book_number']; ?></td>
+    <td><?php echo $book['book_description']; ?></td>
+    <td><?php echo $book['year']; ?></td>
+    <td><?php echo $book['month']; ?></td>
+    <td><?php echo $book['day']; ?></td>
+  </tr>
